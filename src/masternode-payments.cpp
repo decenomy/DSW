3934ac2315fd9ebf113dcfc77ee2fc4b8f32ffa5
Peteracn96 <pacn96@gmail.com>
2020-11-17 11:09:54 +0100

    Correcting block order in function 'FillBlockPayee'

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -369,7 +369,7 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, const CBloc
             txNew.vout.resize(2);
             txNew.vout[1].scriptPubKey = payee;
             txNew.vout[1].nValue = masternodePayment;
-            txNew.vout[0].nValue = GetBlockValue(pindexPrev->nHeight) - masternodePayment;
+            txNew.vout[0].nValue = GetBlockValue(pindexPrev->nHeight + 1) - masternodePayment;
         }
 
         CTxDestination address1;
