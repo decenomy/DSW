ca19fdf88aec1e1d8f5ed8b8511cf185a263e94c
Peteracn96 <pacn96@gmail.com>
2020-11-25 11:18:07 +0100

    Jackpot Core replaced by Jackpot.

--- src/qt/locale/pivx_de.ts
+++ src/qt/locale/pivx_de.ts
@@ -1160,16 +1160,16 @@ Das Staking wird automatisch gestartet, sobald die Wallet genügend Bestätigung
         <translation>Version</translation>
     </message>
     <message>
-        <source>Jackpot Core</source>
-        <translation>Jackpot Core</translation>
+        <source>Jackpot</source>
+        <translation>Jackpot</translation>
     </message>
     <message>
         <source>(%1-bit)</source>
         <translation>(%1-Bit)</translation>
     </message>
     <message>
-        <source>About Jackpot Core</source>
-        <translation>Über Jackpot Core</translation>
+        <source>About Jackpot</source>
+        <translation>Über Jackpot</translation>
     </message>
     <message>
         <source>Command-line options</source>
@@ -1223,16 +1223,16 @@ Das Staking wird automatisch gestartet, sobald die Wallet genügend Bestätigung
         <translation>Willkommen</translation>
     </message>
     <message>
-        <source>Welcome to Jackpot Core.</source>
-        <translation>Willkommen zu Jackpot Core.</translation>
+        <source>Welcome to Jackpot.</source>
+        <translation>Willkommen zu Jackpot.</translation>
     </message>
     <message>
-        <source>As this is the first time the program is launched, you can choose where Jackpot Core will store its data.</source>
-        <translation>Da dies das erste Mal ist, dass Sie Jackpot Core starten, legen Sie jetzt bitte fest, an welchem Ort die Daten gespeichert werden sollen.</translation>
+        <source>As this is the first time the program is launched, you can choose where Jackpot will store its data.</source>
+        <translation>Da dies das erste Mal ist, dass Sie Jackpot starten, legen Sie jetzt bitte fest, an welchem Ort die Daten gespeichert werden sollen.</translation>
     </message>
     <message>
-        <source>Jackpot Core will download and store a copy of the 777 block chain. At least %1GB of data will be stored in this directory, and it will grow over time. The wallet will also be stored in this directory.</source>
-        <translation>Jackpot Core wird die Blockchain laden und lokal speichern. Dafür sind mindestens %1GB freier Speicherplatz erforderlich. Der Speicherbedarf wird mit der Zeit anwachsen. Die Wallet wird ebenfalls in diesem Verzeichnis gespeichert.</translation>
+        <source>Jackpot will download and store a copy of the 777 block chain. At least %1GB of data will be stored in this directory, and it will grow over time. The wallet will also be stored in this directory.</source>
+        <translation>Jackpot wird die Blockchain laden und lokal speichern. Dafür sind mindestens %1GB freier Speicherplatz erforderlich. Der Speicherbedarf wird mit der Zeit anwachsen. Die Wallet wird ebenfalls in diesem Verzeichnis gespeichert.</translation>
     </message>
     <message>
         <source>Use the default data directory</source>
@@ -1251,8 +1251,8 @@ Das Staking wird automatisch gestartet, sobald die Wallet genügend Bestätigung
         <translation>OK</translation>
     </message>
     <message>
-        <source>Jackpot Core</source>
-        <translation>Jackpot Core</translation>
+        <source>Jackpot</source>
+        <translation>Jackpot</translation>
     </message>
     <message>
         <source>Error: Specified data directory "%1" cannot be created.</source>
@@ -2078,8 +2078,8 @@ https://www.transifex.com/pivx-project/pivx-project-translations</translation>
 <context>
     <name>PIVXGUI</name>
     <message>
-        <source>Jackpot Core</source>
-        <translation>Jackpot Core</translation>
+        <source>Jackpot</source>
+        <translation>Jackpot</translation>
     </message>
     <message>
         <source>Wallet</source>
@@ -2106,8 +2106,8 @@ https://www.transifex.com/pivx-project/pivx-project-translations</translation>
         <translation>Anwendung beenden</translation>
     </message>
     <message>
-        <source>Jackpot Core client</source>
-        <translation>Jackpot Core Client</translation>
+        <source>Jackpot client</source>
+        <translation>Jackpot Client</translation>
     </message>
     <message>
         <source>Error</source>
@@ -2311,8 +2311,8 @@ Adresse: %4
         <translation>Ein fataler Fehler ist aufgetreten. 777 kann nicht länger sicher fortfahren und wird geschlossen</translation>
     </message>
     <message>
-        <source>Jackpot Core</source>
-        <translation>Jackpot Core</translation>
+        <source>Jackpot</source>
+        <translation>Jackpot</translation>
     </message>
     <message>
         <source>Error: Specified data directory "%1" does not exist.</source>
@@ -2331,8 +2331,8 @@ Adresse: %4
         <translation>Fehler beim Einlesen der Konfigurationsdatei %1 für den Masternode.</translation>
     </message>
     <message>
-        <source>Jackpot Core didn't yet exit safely...</source>
-        <translation>Jackpot Core konnte noch nicht sicher beendet werden...</translation>
+        <source>Jackpot didn't yet exit safely...</source>
+        <translation>Jackpot konnte noch nicht sicher beendet werden...</translation>
     </message>
     <message>
         <source>Contact</source>
@@ -4778,8 +4778,8 @@ Sind Sie sicher?</translation>
 <context>
     <name>ShutdownWindow</name>
     <message>
-        <source>Jackpot Core is shutting down...</source>
-        <translation>Jackpot Core wird beendet...</translation>
+        <source>Jackpot is shutting down...</source>
+        <translation>Jackpot wird beendet...</translation>
     </message>
     <message>
         <source>Do not shut down the computer until this window disappears.</source>
@@ -4808,8 +4808,8 @@ Sind Sie sicher?</translation>
         <translation>Lade...</translation>
     </message>
     <message>
-        <source>Jackpot Core</source>
-        <translation>Jackpot Core</translation>
+        <source>Jackpot</source>
+        <translation>Jackpot</translation>
     </message>
 </context>
 <context>
@@ -5346,8 +5346,8 @@ Wählen Sie weniger Eingänge über coin control aus.</translation>
     </message>
     <message>
         <source>Welcome to
-Jackpot Core Wallet</source>
-        <translation>Willkommen beim Jackpot Core Wallet</translation>
+Jackpot Wallet</source>
+        <translation>Willkommen beim Jackpot Wallet</translation>
     </message>
     <message>
         <source>777 is the world’s most innovative Proof of Stake blockchain based technology. Developed by a team of highly experienced developers and cryptographers.</source>
@@ -5397,8 +5397,8 @@ Jackpot Core Wallet</source>
         <translation>Binde an die angegebene Adresse und höre auf JSON-RPC verbindungen. Benutze [host]:port Notation für IPv6. Diese Option kann mehrere Male aufgerufen werden (Standard: Binde an alle Schnittstellen)</translation>
     </message>
     <message>
-        <source>Cannot obtain a lock on data directory %s. Jackpot Core is probably already running.</source>
-        <translation>Kann keine Sperre für das Datenverzeichnis %s erhalten. Jackpot Core läuft wahrscheinlich bereits.</translation>
+        <source>Cannot obtain a lock on data directory %s. Jackpot is probably already running.</source>
+        <translation>Kann keine Sperre für das Datenverzeichnis %s erhalten. Jackpot läuft wahrscheinlich bereits.</translation>
     </message>
     <message>
         <source>Change automatic finalized budget voting behavior. mode=auto: Vote for only exact finalized budget match to my generated budget. (string, default: auto)</source>
@@ -5561,8 +5561,8 @@ Jackpot Core Wallet</source>
         <translation>Gesamtlänge des Netzwerkversionsstring (%i) überschreitet die Länge (%i). Reduzieren Sie die Zahl oder die Größe des Dokuments.</translation>
     </message>
     <message>
-        <source>Unable to bind to %s on this computer. Jackpot Core is probably already running.</source>
-        <translation>Fehler: Port %s ist bereits belegt! Läuft bereits eine andere Jackpot Core Wallet ?</translation>
+        <source>Unable to bind to %s on this computer. Jackpot is probably already running.</source>
+        <translation>Fehler: Port %s ist bereits belegt! Läuft bereits eine andere Jackpot Wallet ?</translation>
     </message>
     <message>
         <source>Use separate SOCKS5 proxy to reach peers via Tor hidden services (default: %s)</source>
@@ -5577,8 +5577,8 @@ Jackpot Core Wallet</source>
         <translation>Warnung: -paytxfee ist sehr hoch eingestellt! Diese Transaktionsgebühr werden Ihnen abgebucht, falls Sie die Transaktion überweisen.</translation>
     </message>
     <message>
-        <source>Warning: Please check that your computer's date and time are correct! If your clock is wrong Jackpot Core will not work properly.</source>
-        <translation>Warnung: Bitte stellen Sie sicher, das vom Computer verwendete Zeit und Datumangaben korrekt sind! Wenn ihr System falsche Zeitangaben nutzt, wird Jackpot Core nicht korrekt funktionieren.</translation>
+        <source>Warning: Please check that your computer's date and time are correct! If your clock is wrong Jackpot will not work properly.</source>
+        <translation>Warnung: Bitte stellen Sie sicher, das vom Computer verwendete Zeit und Datumangaben korrekt sind! Wenn ihr System falsche Zeitangaben nutzt, wird Jackpot nicht korrekt funktionieren.</translation>
     </message>
     <message>
         <source>Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.</source>
@@ -5793,8 +5793,8 @@ Jackpot Core Wallet</source>
         <translation>Hinweis</translation>
     </message>
     <message>
-        <source>Initialization sanity check failed. Jackpot Core is shutting down.</source>
-        <translation>Initialisierung Sanity-Check fehlgeschlagen. Jackpot Core schaltet ab.</translation>
+        <source>Initialization sanity check failed. Jackpot is shutting down.</source>
+        <translation>Initialisierung Sanity-Check fehlgeschlagen. Jackpot schaltet ab.</translation>
     </message>
     <message>
         <source>Insufficient funds</source>
@@ -6131,8 +6131,8 @@ Akzeptiere keine Transaktion, deren vorherige Größe mit &lt;n&gt; im Mempool �
         <translation>Minimaler positiver Betrag (in 777), der von GUI und RPC für den Stake split Schwellenwert zugelassen wird (Standard: %s)</translation>
     </message>
     <message>
-        <source>Please check that your computer's date and time are correct! If your clock is wrong Jackpot Core will not work properly.</source>
-        <translation>Bitte stellen Sie sicher, das vom Computer verwendete Zeit und Datumangaben korrekt sind! Wenn ihr System falsche Zeitangaben nutzt, wird Jackpot Core nicht korrekt funktionieren.</translation>
+        <source>Please check that your computer's date and time are correct! If your clock is wrong Jackpot will not work properly.</source>
+        <translation>Bitte stellen Sie sicher, das vom Computer verwendete Zeit und Datumangaben korrekt sind! Wenn ihr System falsche Zeitangaben nutzt, wird Jackpot nicht korrekt funktionieren.</translation>
     </message>
     <message>
         <source>Query for peer addresses via DNS lookup, if low on addresses (default: 1 unless -connect/-noconnect)</source>
@@ -6544,8 +6544,8 @@ Der Schwellenwert darf nicht kleiner sein als %s</translation>
         <translation>Wallet %s liegt außerhalb des Datenverzeichnisses %s</translation>
     </message>
     <message>
-        <source>Wallet needed to be rewritten: restart Jackpot Core to complete</source>
-        <translation>Wallet musste neu geschrieben werden: Bitte Jackpot Core neu starten</translation>
+        <source>Wallet needed to be rewritten: restart Jackpot to complete</source>
+        <translation>Wallet musste neu geschrieben werden: Bitte Jackpot neu starten</translation>
     </message>
     <message>
         <source>Wallet options:</source>
