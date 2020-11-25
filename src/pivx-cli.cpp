0ad03e9e4a0673219084e0b95d5e237fd3c396be
Peteracn96 <pacn96@gmail.com>
2020-11-25 20:24:49 +0100

    Replacing 'PIVX Core' by 'Jackpot' in the command line (cli).

--- src/pivx-cli.cpp
+++ src/pivx-cli.cpp
@@ -72,10 +72,10 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc < 2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("PIVX Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Jackpot RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  pivx-cli [options] <command> [params]  " + _("Send command to PIVX Core") + "\n" +
+                        "  pivx-cli [options] <command> [params]  " + _("Send command to Jackpot") + "\n" +
                         "  pivx-cli [options] help                " + _("List commands") + "\n" +
                         "  pivx-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
