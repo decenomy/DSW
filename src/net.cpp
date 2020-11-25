ea1c5aff3acef5c813b6c2e8c3fe623ccc6d2939
Peteracn96 <pacn96@gmail.com>
2020-11-25 19:50:17 +0100

    Replacing 'PIVX Core' by 'Jackpot' in probably already running error message.

--- src/net.cpp
+++ src/net.cpp
@@ -1983,7 +1983,7 @@ bool CConnman::BindListenPort(const CService& addrBind, std::string& strError, b
     if (::bind(hListenSocket, (struct sockaddr*)&sockaddr, len) == SOCKET_ERROR) {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. PIVX Core is probably already running."), addrBind.ToString());
+            strError = strprintf(_("Unable to bind to %s on this computer. Jackpot is probably already running."), addrBind.ToString());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %s)"), addrBind.ToString(), NetworkErrorString(nErr));
         LogPrintf("%s\n", strError);
