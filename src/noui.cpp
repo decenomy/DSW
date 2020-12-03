16ca6b4b995f9fd4105aa87cd9ae002d0fc8e65c
João Águas <jmsaguas@gmail.com>
2020-12-03 15:21:45 +0100

    Changed text, and the multiples of the units

--- src/noui.cpp
+++ src/noui.cpp
@@ -49,7 +49,7 @@ static void noui_InitMessage(const std::string& message)
 
 void noui_connect()
 {
-    // Connect pivxd signal handlers
+    // Connect jackpotd signal handlers
     uiInterface.ThreadSafeMessageBox.connect(noui_ThreadSafeMessageBox);
     uiInterface.InitMessage.connect(noui_InitMessage);
 }
