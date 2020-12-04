2af2a25f17e6289fd74ee15be535f86895dcf47f
João Águas <jmsaguas@gmail.com>
2020-12-04 19:35:42 +0100

    Changed the URL to jackpots site.

--- src/pivxd.cpp
+++ src/pivxd.cpp
@@ -29,7 +29,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Jackpot (http://www.pivx.org),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Jackpot (https://777coin.win/),
  * which enables instant payments to anyone, anywhere in the world. Jackpot uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
