13b3a94665e210e220c654d3a6bcbf255a7ac9fb
João Águas <jmsaguas@gmail.com>
2020-12-04 19:41:16 +0100

    changed output text prefix pivx -> jackpot

--- src/util.h
+++ src/util.h
@@ -183,7 +183,7 @@ void SetThreadPriority(int nPriority);
 template <typename Callable>
 void TraceThread(const char* name, Callable func)
 {
-    std::string s = strprintf("pivx-%s", name);
+    std::string s = strprintf("jackpot-%s", name);
     util::ThreadRename(s.c_str());
     try {
         LogPrintf("%s thread start\n", name);
