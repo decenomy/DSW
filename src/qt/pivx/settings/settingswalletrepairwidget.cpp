a2d704c8384aa646d5dae250df3f12bfd567165f
João Águas <jmsaguas@gmail.com>
2020-12-04 20:15:51 +0100

    Changed prefix PIVX -> Jackpot

--- src/qt/pivx/settings/settingswalletrepairwidget.cpp
+++ src/qt/pivx/settings/settingswalletrepairwidget.cpp
@@ -119,7 +119,7 @@ void SettingsWalletRepairWidget::buildParameterlist(QString arg)
     // Append repair parameter to command line.
     args.append(arg);
 
-    // Send command-line arguments to PIVXGUI::handleRestart()
+    // Send command-line arguments to JackpotGUI::handleRestart()
     Q_EMIT handleRestart(args);
 }
 
