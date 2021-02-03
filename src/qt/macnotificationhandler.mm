896ecedf51ac8ee65af37bdc5544153625745a94
Pedro Simões <pedro@decenomy.net>
2021-02-03 19:34:34 +0000

    mac fixes

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -15,7 +15,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.pivx.Pivx-Qt";
+        return @"io.jackpot.Jackpot-Qt";
     } else {
         return [self __bundleIdentifier];
     }
