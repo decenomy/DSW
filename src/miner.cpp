ccaf0cd154ab5ad03503d6daf24c9a2b814fd5fb
Lot Cooler <35534106+LotCooler@users.noreply.github.com>
2021-02-05 17:17:57 -0800

    fix stuck fMasternodeSync value
    fMasternodeSync was getting stuck and not being re-evaluated in while loop that was waiting for wallet to be ready to enable staking

--- src/miner.cpp
+++ src/miner.cpp
@@ -559,6 +559,12 @@ bool fStakeableCoins = false;
 bool fMasternodeSync = false;
 int nMintableLastCheck = 0;
 
+bool IsMasternodeSync()
+{
+    fMasternodeSync = sporkManager.IsSporkActive(SPORK_106_STAKING_SKIP_MN_SYNC) || !masternodeSync.NotCompleted();
+    return fMasternodeSync;
+}
+
 void CheckForCoins(CWallet* pwallet, const int minutes, std::vector<COutput>* availableCoins)
 {
     //control the amount of times the client will check for mintable coins
@@ -566,7 +572,7 @@ void CheckForCoins(CWallet* pwallet, const int minutes, std::vector<COutput>* av
     if ((nTimeNow - nMintableLastCheck > minutes * 60)) {
         nMintableLastCheck = nTimeNow;
         fStakeableCoins = pwallet->StakeableCoins(availableCoins);
-        fMasternodeSync = sporkManager.IsSporkActive(SPORK_106_STAKING_SKIP_MN_SYNC) || !masternodeSync.NotCompleted();
+        IsMasternodeSync();  //function re-evaluates fMasternodeSync and returns it but we don't need the return value here, so ignore it
     }
 }
 
@@ -604,7 +610,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
             // update fStakeableCoins (5 minute check time);
             CheckForCoins(pwallet, 5, &availableCoins);
 
-            while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers()) || pwallet->IsLocked() || !fStakeableCoins || !fMasternodeSync) {
+            while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers()) || pwallet->IsLocked() || !fStakeableCoins || !IsMasternodeSync()) {
                 MilliSleep(5000);
                 // Do a separate 1 minute check here to ensure fStakeableCoins is updated
                 if (!fStakeableCoins) CheckForCoins(pwallet, 1, &availableCoins);
