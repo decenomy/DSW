d3f21f5fdb8d5ac15308c9dadc2e0078ac36a009
Pedro Simões <pedro@decenomy.net>
2021-02-13 20:13:44 +0000

    adds build files

---
+++ build_windows.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo -e "\033[0;32mHow many CPU cores do you want to be used in compiling process? (Default is 1. Press enter for default.)\033[0m"
+read -e CPU_CORES
+if [ -z "$CPU_CORES" ]
+then
+	CPU_CORES=1
+fi
+
+# Upgrade the system and install required dependencies
+	sudo apt update
+	sudo apt install git zip unzip build-essential libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 curl g++-mingw-w64-x86-64 libqt5svg5-dev -y
+	echo "1" | sudo update-alternatives --config x86_64-w64-mingw32-g++
+
+# Clone code from SAPP official Github repository
+	git clone https://github.com/CryptoFlowCoin/CryptoFlowCoin.git
+
+# Entering directory
+	cd CryptoFlowCoin
+
+# Compile dependencies
+	cd depends
+	chmod +x config.sub
+	chmod +x config.guess
+	make -j$(echo $CPU_CORES) HOST=x86_64-w64-mingw32 
+	cd ..
+
+# Compile
+	chmod +x share/genbuild.sh
+	chmod +x autogen.sh
+	./autogen.sh
+	./configure --prefix=$(pwd)/depends/x86_64-w64-mingw32 --disable-debug --disable-tests --disable-bench --disable-online-rust CFLAGS="-O3" CXXFLAGS="-O3"
+	make -j$(echo $CPU_CORES) HOST=x86_64-w64-mingw32
+	cd ..
+
+# Create zip file of binaries
+	cp CryptoFlowCoin/src/cryptoflowd.exe CryptoFlowCoin/src/cryptoflow-cli.exe CryptoFlowCoin/src/cryptoflow-tx.exe CryptoFlowCoin/src/qt/cryptoflow-qt.exe .
+	zip CryptoFlow-Windows.zip cryptoflowd.exe cryptoflow-cli.exe cryptoflow-tx.exe cryptoflow-qt.exe
+	rm -f cryptoflowd.exe cryptoflow-cli.exe cryptoflow-tx.exe cryptoflow-qt.exe
