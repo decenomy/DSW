74df23c98a5265f64d79e4ac816cebdc5743413c
perseu <jmsaguas@gmail.com>
2021-02-27 11:32:25 +0100

    Changing color settings on the GUI.

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -665,8 +665,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
     set1 = new QBarSet("z" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(92,75,125));
-    set1->setColor(QColor(176,136,255));
+    set0->setColor(QColor(255,227,164));
+    set1->setColor(QColor(255,227,164));
 
     if (!series) {
         series = new QBarSeries();
--- src/qt/pivx/qtutils.cpp
+++ src/qt/pivx/qtutils.cpp
@@ -215,17 +215,17 @@ QColor getRowColor(bool isLightTheme, bool isHovered, bool isSelected)
 {
     if (isLightTheme) {
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#ffde8a");
         } else if (isHovered) {
-            return QColor("#25bababa");
+            return QColor("#ffde8a");
         } else {
             return QColor("#ffffff");
         }
     } else {
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#707070");
         } else if (isHovered) {
-            return QColor("#25bababa");
+            return QColor("#707070");
         } else {
             return QColor("#0f0b16");
         }
