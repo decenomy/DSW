b3954c830da57915010254188e6053285e5bfea7
Bedri Özgür Güler <bedriguler@gmail.com>
2021-03-02 15:16:13 +0100

    Cleanup

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -136,15 +136,6 @@ UniValue getinfo(const JSONRPCRequest& request)
     }
 
     obj.push_back(Pair("moneysupply",ValueFromAmount(nMoneySupply)));
-    // UniValue zpivObj(UniValue::VOBJ);
-    // for (auto denom : libzerocoin::zerocoinDenomList) {
-    //     if (mapZerocoinSupply.empty())
-    //         zpivObj.push_back(Pair(std::to_string(denom), ValueFromAmount(0)));
-    //     else
-    //         zpivObj.push_back(Pair(std::to_string(denom), ValueFromAmount(mapZerocoinSupply.at(denom) * (denom*COIN))));
-    // }
-    // zpivObj.push_back(Pair("total", ValueFromAmount(GetZerocoinSupply())));
-    // obj.push_back(Pair("zCFLsupply", zpivObj));
 
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
