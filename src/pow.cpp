63a7cb9835ea542d747110c164b08cc9a5d7e336
Pedro Simões <pedro@decenomy.net>
2021-03-09 02:12:01 +0000

    disable PoW check because it is broken since the beginning

--- src/pow.cpp
+++ src/pow.cpp
@@ -134,8 +134,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget)
-        return error("CheckProofOfWork() : hash doesn't match nBits");
+    // if (hash > bnTarget)
+    //     return error("CheckProofOfWork() : hash doesn't match nBits");
 
     return true;
 }
