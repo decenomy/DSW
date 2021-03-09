e967aea598e3c93290cc15f173aba171b49507e3
Peteracn96 <pacn96@gmail.com>
2021-03-09 19:09:47 +0100

    Replace standard coin unit __DSW__ by UCR

--- src/qt/editaddressdialog.cpp
+++ src/qt/editaddressdialog.cpp
@@ -104,7 +104,7 @@ void EditAddressDialog::accept()
             break;
         case AddressTableModel::INVALID_ADDRESS:
             QMessageBox::warning(this, windowTitle(),
-                tr("The entered address \"%1\" is not a valid __DSW__ address.").arg(ui->addressEdit->text()),
+                tr("The entered address \"%1\" is not a valid UCR address.").arg(ui->addressEdit->text()),
                 QMessageBox::Ok, QMessageBox::Ok);
             break;
         case AddressTableModel::DUPLICATE_ADDRESS:
