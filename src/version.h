a6d46b60dffb4e4e384edc2bed2dcbb4ab055a9c
Bedri Özgür Güler <bedriguler@gmail.com>
2021-03-10 06:08:40 +0100

    Cleanup unnecessary constants from version.h

--- src/version.h
+++ src/version.h
@@ -20,10 +20,6 @@ static const int INIT_PROTO_VERSION = 209;
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 70000;
 
-//! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71007;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71008;
-
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70017;
 
