c49ee08254a3c3d057ab924df93d8a2586708546
LotCooler <don.knudsen@gmail.com>
2021-03-09 20:22:05 -0800

    normal color changes not in CSS
    All the standard wallet color changes that are not contained in the CSS files.

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -675,8 +675,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
 	set1 = new QBarSet("MN_" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(92,75,125));
-	set1->setColor(QColor(102,102,255));
+    set0->setColor(QColor(197,247,255));
+	set1->setColor(QColor(123,230,255));
 
     if (!series) {
         series = new QBarSeries();
