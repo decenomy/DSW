571ca1fa63bba88bb07c8f053f975c03ef4a8653
Pedro Simões <pedro@decenomy.net>
2021-03-17 18:34:23 +0000

    chenges some port values

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -193,7 +193,7 @@ UniValue addnode(const JSONRPCRequest& request)
             "2. \"command\"  (string, required) 'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once\n"
 
             "\nExamples:\n" +
-            HelpExampleCli("addnode", "\"192.168.0.6:3333\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:3333\", \"onetry\""));
+            HelpExampleCli("addnode", "\"192.168.0.6:13333\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:13333\", \"onetry\""));
 
     if(!g_connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
@@ -261,7 +261,7 @@ UniValue getaddednodeinfo(const JSONRPCRequest& request)
             "    \"connected\" : true|false,          (boolean) If connected\n"
             "    \"addresses\" : [                    (list of objects) Only when connected = true\n"
             "       {\n"
-            "         \"address\" : \"192.168.0.201:3333\",  (string) The cryptoflow server IP and port we're connected to\n"
+            "         \"address\" : \"192.168.0.201:13333\",  (string) The cryptoflow server IP and port we're connected to\n"
             "         \"connected\" : \"outbound\"           (string) connection, inbound or outbound\n"
             "       }\n"
             "     ]\n"
