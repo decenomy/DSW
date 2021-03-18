3c3a41254fc029a66e8cbb7c61e0f1440c8256be
Bedri Özgür Güler <bedriguler@gmail.com>
2021-03-18 18:01:30 +0100

    Rebrands some forgotten old RPC ports

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -11,9 +11,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-    access = ServiceProxy("http://127.0.0.1:30002")
+    access = ServiceProxy("http://127.0.0.1:31002")
 else:
-    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:30002")
+    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:31002")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -605,7 +605,7 @@ std::string HelpExampleRpc(std::string methodname, std::string args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
            "\"method\": \"" +
-           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:30002/\n";
+           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:31002/\n";
 }
 
 void RPCSetTimerInterfaceIfUnset(RPCTimerInterface *iface)
