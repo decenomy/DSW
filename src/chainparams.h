5dd73c2a4aacec5c96efc912780920093f57a864
Mustafa Ali KAHRAMAN <tafali@gmail.com>
2021-03-21 23:17:37 +0300

    replace port placeholders and set defines

--- src/chainparams.h
+++ src/chainparams.h
@@ -9,9 +9,9 @@
 #ifndef BITCOIN_CHAINPARAMS_H
 #define BITCOIN_CHAINPARAMS_H
 
-#define __PORT_MAINNET__ 9999
-#define __PORT_TESTNET__ 19999
-#define __PORT_REGTEST__ 29999
+#define __PORT_MAINNET__ 14725
+#define __PORT_TESTNET__ 14726
+#define __PORT_REGTEST__ 14728
 
 #include "chainparamsbase.h"
 #include "checkpoints.h"
