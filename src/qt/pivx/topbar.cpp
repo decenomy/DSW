04c33857dfe279b8c0e651d36961aea2a32b8830
Peteracn96 <pacn96@gmail.com>
2021-03-24 13:27:35 +0100

    Replace standard coin name __decenomy__ by jackpot (3)

--- src/qt/pivx/topbar.cpp
+++ src/qt/pivx/topbar.cpp
@@ -83,7 +83,7 @@ TopBar::TopBar(PIVXGUI* _mainWindow, QWidget *parent) :
     ui->pushButtonStack->setButtonText(tr("Staking Disabled"));
 
     ui->pushButtonConf->setButtonClassStyle("cssClass", "btn-check-conf");
-    ui->pushButtonConf->setButtonText("__decenomy__.conf");
+    ui->pushButtonConf->setButtonText("jackpot.conf");
     ui->pushButtonConf->setChecked(false);
 
     ui->pushButtonMasternodes->setButtonClassStyle("cssClass", "btn-check-masternodes");
@@ -348,7 +348,7 @@ void TopBar::onBtnConfClicked()
     ui->pushButtonConf->setChecked(false);
 
     if (!GUIUtil::openConfigfile())
-        inform(tr("Unable to open __decenomy__.conf with default application"));
+        inform(tr("Unable to open jackpot.conf with default application"));
 }
 
 void TopBar::onBtnMasternodesClicked()
--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -476,7 +476,7 @@ WalletModel::SendCoinsReturn WalletModel::sendCoins(WalletModelTransaction& tran
                 std::string value;
                 rcp.paymentRequest.SerializeToString(&value);
                 newTx->vOrderForm.push_back(std::make_pair(key, value));
-            } else if (!rcp.message.isEmpty()) // Message from normal __decenomy__:URI (__decenomy__:XyZ...?message=example)
+            } else if (!rcp.message.isEmpty()) // Message from normal jackpot:URI (jackpot:XyZ...?message=example)
             {
                 newTx->vOrderForm.push_back(std::make_pair("Message", rcp.message.toStdString()));
             }
