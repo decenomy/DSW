60111176b6c849d17d0f0db1b94f00234f5ed122
LotCooler <don.knudsen@gmail.com>
2021-03-25 10:46:05 -0700

    resolving hashing and jenkins file update commits
    manually doing these commits in this branch to resolve conflicts with DASHD-develop
    https://github.com/decenomy/DSW/commit/92dbe4c736bd2b3328bee5b59911c788b0a4cca9
    https://github.com/decenomy/DSW/commit/67353555755019e03be01d63eb009a44748c4822
    https://github.com/decenomy/DSW/commit/b574b78eedea5afb8dc6ef13860f1574b62d39ae

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -14,9 +14,10 @@
 #include "utilstrencodings.h"
 #include "util.h"
 
+// TODO: Change X11KVS algorithm call to whatever the coin being adapted is used.
 uint256 CBlockHeader::GetHash() const
 {
-     if (nVersion < 4)  {
+     if (nVersion < 4)  { // nVersion = 1, 2, 3
 #if defined(WORDS_BIGENDIAN)
         uint8_t data[80];
         WriteLE32(&data[0], nVersion);
@@ -25,14 +26,14 @@ uint256 CBlockHeader::GetHash() const
         WriteLE32(&data[68], nTime);
         WriteLE32(&data[72], nBits);
         WriteLE32(&data[76], nNonce);
+
         return XEVAN(data, data + 80);
 #else // Can take shortcut for little endian
         return XEVAN(BEGIN(nVersion), END(nNonce));
 #endif
     }
-    // version >= 4
-    return SerializeHash(*this);
-
+	
+    return SerializeHash(*this); // nVersion >= 4
 }
 
 std::string CBlock::ToString() const
