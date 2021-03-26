9844bb2edbe98ceeb84ba08cfbbc193f2e39aa82
LotCooler <don.knudsen@gmail.com>
2021-03-25 23:40:50 -0700

    initial DASHD design changes
    first stab at DASHD design changes

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -678,8 +678,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
 	set1 = new QBarSet("MN_" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(92,75,125));
-	set1->setColor(QColor(102,102,255));
+    set0->setColor(QColor(140,178,68));
+    set1->setColor(QColor(136,207,255));
 
     if (!series) {
         series = new QBarSeries();
--- src/qt/pivx/qtutils.cpp
+++ src/qt/pivx/qtutils.cpp
@@ -215,17 +215,17 @@ QColor getRowColor(bool isLightTheme, bool isHovered, bool isSelected)
 {
     if (isLightTheme) {
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#f9f9f9");
         } else if (isHovered) {
-            return QColor("#25bababa");
+            return QColor("#f9f9f9");
         } else {
             return QColor("#ffffff");
         }
     } else {
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#212121");
         } else if (isHovered) {
-            return QColor("#25bababa");
+            return QColor("#212121");
         } else {
             return QColor("#0f0b16");
         }
