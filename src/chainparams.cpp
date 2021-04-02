67a0e3941e3b3fc26dbcccd06b23412964e22f1d
Pedro Simões <pedro@decenomy.net>
2021-04-02 16:34:02 +0100

    adjusts seed servers and adds a seeder

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -253,14 +253,15 @@ public:
         nDefaultPort = __PORT_MAINNET__;
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.emplace_back(CDNSSeedData("seed1.sappcoin.com", "seed1.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed2.sappcoin.com", "seed2.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed3.sappcoin.com", "seed3.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed4.sappcoin.com", "seed4.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed5.sappcoin.com", "seed5.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed6.sappcoin.com", "seed6.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed7.sappcoin.com", "seed7.sappcoin.com", true));
-        vSeeds.emplace_back(CDNSSeedData("seed8.sappcoin.com", "seed8.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seeder", "seeder.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed1", "seed1.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed2", "seed2.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed3", "seed3.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed4", "seed4.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed5", "seed5.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed6", "seed6.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed7", "seed7.sappcoin.com", true));
+        vSeeds.emplace_back(CDNSSeedData("seed8", "seed8.sappcoin.com", true));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 63); // 7
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 18); // 7
