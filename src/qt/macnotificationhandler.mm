c5f57c0cbbf983d58dd70d21fc212a4aa6a24124
perseu <jmsaguas@gmail.com>
2021-04-09 12:39:19 +0200

    Updated text.

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -15,7 +15,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.__decenomy__.__Decenomy__-Qt";
+        return @"io.__decenomy__.Peony-Qt";
     } else {
         return [self __bundleIdentifier];
     }
