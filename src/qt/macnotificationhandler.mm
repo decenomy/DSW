88218e2c22df69fc0b5490a533a66a63e69556fe
Peteracn96 <pacn96@gmail.com>
2021-04-09 17:36:20 +0200

    Replace standard coin name __decenomy__ by beacon (9)

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -15,7 +15,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.__decenomy__.Beacon-Qt";
+        return @"io.beacon.Beacon-Qt";
     } else {
         return [self __bundleIdentifier];
     }
