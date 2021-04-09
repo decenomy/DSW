05951b78cb187adce36bdf91cc57c8e6076f9e65
perseu <jmsaguas@gmail.com>
2021-04-09 14:05:30 +0200

    Updated text.

--- test/functional/rpc_budget.py
+++ test/functional/rpc_budget.py
@@ -42,8 +42,8 @@ class BudgetProposalTest(PivxTestFramework):
         assert_raises_rpc_error(-8, "Invalid block start", self.nodes[0].preparebudget,
                                 name, scheme + url, numcycles, nextsuperblock - budgetcycleblocks, address, cycleamount)
 
-        self.log.info("Test with invalid __Decenomy__ address")
-        assert_raises_rpc_error(-5, "Invalid __Decenomy__ address", self.nodes[0].preparebudget,
+        self.log.info("Test with invalid Peony address")
+        assert_raises_rpc_error(-5, "Invalid Peony address", self.nodes[0].preparebudget,
                                 name, scheme + url, numcycles, nextsuperblock, "DBREvBPNQguwuC4YMoCG5FoH1sA2YntvZm", cycleamount)
 
         self.log.info("Test with too low amount")
