5b435ef605d4e7a4d56d189af137803a370e18b3
Pedro Simões <pedro@decenomy.net>
2021-04-12 11:00:14 +0100

    disables the AA_UseHighDpiPixmaps QT flag

--- src/qt/pivx.cpp
+++ src/qt/pivx.cpp
@@ -544,10 +544,11 @@ int main(int argc, char* argv[])
     Q_INIT_RESOURCE(pivx_locale);
     Q_INIT_RESOURCE(pivx);
 
+    //TODO to set this up we need to review all the UI measurements to be relative to the screen DPI
     // Generate high-dpi pixmaps
-    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    //QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #if QT_VERSION >= 0x050600
-    //QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling); //TODO to set this up we need to review all the UI measurements to be relative to the screen DPI
+    //QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling); 
 #endif
 #ifdef Q_OS_MAC
     QApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
