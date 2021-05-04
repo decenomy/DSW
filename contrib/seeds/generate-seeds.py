132adaf0e470b60bb794fbcb98b80aebea92363c
Peteracn96 <pacn96@gmail.com>
2021-05-04 18:14:21 +0200

    Replacing testnet port placeholder

--- contrib/seeds/generate-seeds.py
+++ contrib/seeds/generate-seeds.py
@@ -131,7 +131,7 @@ def main():
         process_nodes(g, f, 'pnSeed6_main', 36552)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'), 'r', encoding="utf8") as f:
-        process_nodes(g, f, 'pnSeed6_test', __PORT_TESTNET__)
+        process_nodes(g, f, 'pnSeed6_test', 46552)
     g.write('#endif // BITCOIN_CHAINPARAMSSEEDS_H\n')
 
 if __name__ == '__main__':
