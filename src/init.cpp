b70855cdc3d4dd50b27b33bb21f5ded0f04a0172
Peteracn96 <pacn96@gmail.com>
2021-05-05 16:33:42 +0200

    Add 2020 year to the copyrights

--- src/init.cpp
+++ src/init.cpp
@@ -571,7 +571,7 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2015-%i The PIVX Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
-           FormatParagraph(strprintf(_("Copyright (C) %i The Decenomy Core Developers"), COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2020-%i The Decenomy Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
