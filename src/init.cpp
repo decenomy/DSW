214a967d207e215011eac1ed24321d22c36d5b41
perseu <jmsaguas@gmail.com>
2021-05-21 18:37:12 +0200

    Updated text.

--- src/init.cpp
+++ src/init.cpp
@@ -927,7 +927,7 @@ void InitLogging()
     LogPrintf("CryptoSaga version %s (%s)\n", version_string, CLIENT_DATE);
 }
 
-/** Initialize __decenomy__.
+/** Initialize cryptosaga.
  *  @pre Parameters should be parsed and config file should be read.
  */
 bool AppInit2()
