3a943de2e832acdffe92b695071959ef2fe189ba
perseu <jmsaguas@gmail.com>
2021-05-24 16:02:59 +0200

    Changed the name of port related constants

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams() 
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = __RPCPORT_MAINNET__;
+        nRPCPort = RPCPORT_MAINNET;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = __RPCPORT_TESTNET__;
+        nRPCPort = RPCPORT_TESTNET;
         strDataDir = "testnet1";
     }
 };
