1f05f10ed06326601af371cc6f583560fa9c29d9
pedro-at-decenomy <pedro@decenomy.net>
2021-06-06 18:19:12 +0100

    add previously banned addresses

--- src/main.cpp
+++ src/main.cpp
@@ -99,6 +99,32 @@ bool fCheckBlockIndex = false;
 bool fVerifyingBlocks = false;
 size_t nCoinCacheUsage = 5000 * 300;
 
+// banned addresses
+std::set<std::string> bannedAddresses = {
+    "Sd2xcwvvtRH8P8sLemSiPjadTfBd9myPbW",
+    "STFSLXfG31Xr8Symk78aG6jCu391yp8kWD",
+    "SbPoXEFrMJwes3mysfgGzP2mAtw1SmrC7H",
+    "ScM5iV4aJEwMipWGHHcS8E1qLsarwk6DuK",
+    "Sfv6SUgcSgmmpwp3UypfYgnK1x97rfC9Dj",
+    "SY6UdUKC8yxci8vXgvQYMgeUNRDvymEhM3",
+    "SdaX6DR3gdpakcFrKJfCDB6GJjC4J9XJ8M",
+    "Sh412EAoGLvn1WnTUCZbHiUGCk2dzmdQoA",
+    "ST74xpmzemL4ELiBpyDmirzgahujSUiYmM",
+    "SaWmWbLSghhn8JAE8JQfdLQy9cvf1ADKUD",
+    "Sic7sZBNkijna4zNLSVgTBkfr2ebP6c9wk",
+    "Sh8N9R2Li5Wm5B7g3xxfEotp9Vpp38baJM",
+    "SVAjKY5p9NPSNwG7PLK3VzeXUdJjm2W7CY",
+    "SQfMZVatpQR9b3KdKp992nxeEZNWkcz7d2",
+    "SNAgLi7pfHD6BDAkQQ74ixtT4o59wkqP8Y",
+    "SS6ZgTuvafGX98YqeHdu79wpGrR1KxuqMw",
+    "SMoP6U7uazpLdqZ18GQFVNNuV77UTK16wh",
+    "SjfZFjCv2PxNKQeDgW1RmsFjSpq5PngaZc",
+    "SPop7eX3kMjwojy1k1EHAqBoodhbski4tR",
+    "Sf3KBrFqmD3PBHrwTpdsFgUTctEz9mQn5Z",
+};
+
+int bannedAddressesStartHeight = 586593;
+
 /* If the tip is older than this (in seconds), the node is considered to be in initial block download. */
 int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
 
@@ -875,6 +901,23 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState &state, const C
     uint256 hash = tx.GetHash();
     if (pool.exists(hash)) {
         return state.Invalid(false, REJECT_ALREADY_KNOWN, "txn-already-in-mempool");
+    }
+
+    // ----------- banned transaction scanning -----------
+    if (!bannedAddresses.empty()) {
+        for (unsigned int i = 0; i < tx.vin.size(); ++i) {
+            uint256 hashBlock;
+            CTransaction txPrev;
+            if (GetTransaction(tx.vin[i].prevout.hash, txPrev, hashBlock, true)) { // get the vin's previous transaction
+                CTxDestination source;
+                if (ExtractDestination(txPrev.vout[tx.vin[i].prevout.n].scriptPubKey, source)) { // extract the destination of the previous transaction's vout[n]
+                    std::string addr = EncodeDestination(source);
+                    if (bannedAddresses.find(addr) != bannedAddresses.end()) {
+                        return state.DoS(0, false, REJECT_INVALID, "bad-txns-invalid-outputs");
+                    }
+                }
+            }
+        }
     }
 
     bool hasZcSpendInputs = tx.HasZerocoinSpendInputs();
@@ -3556,10 +3599,33 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
     const int nHeight = pindexPrev == nullptr ? 0 : pindexPrev->nHeight + 1;
 
     // Check that all transactions are finalized
-    for (const CTransaction& tx : block.vtx)
+    for (const CTransaction& tx : block.vtx) {
         if (!IsFinalTx(tx, nHeight, block.GetBlockTime())) {
             return state.DoS(10, false, REJECT_INVALID, "bad-txns-nonfinal", false, "non-final transaction");
         }
+    }
+
+    // Check that all transactions are not banned
+    if (nHeight > bannedAddressesStartHeight && !bannedAddresses.empty()) {
+        for (const CTransaction& tx : block.vtx) {
+            if (!tx.IsCoinBase()) {
+                for (unsigned int i = 0; i < tx.vin.size(); ++i) {
+                    uint256 hashBlock;
+                    CTransaction txPrev;
+                    if (GetTransaction(tx.vin[i].prevout.hash, txPrev, hashBlock, true)) { // get the vin's previous transaction
+                        CTxDestination source;
+                        if (ExtractDestination(txPrev.vout[tx.vin[i].prevout.n].scriptPubKey, source)) { // extract the destination of the previous transaction's vout[n]
+                            std::string addr = EncodeDestination(source);
+
+                            if (bannedAddresses.find(addr) != bannedAddresses.end()) {
+                                return state.DoS(100, error("%s : Banned address %s tried to send a transaction %s (rejecting it).", __func__, addr.c_str(), txPrev.GetHash().ToString().c_str()), REJECT_INVALID, "bad-txns-banned");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     // // Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
     // if (pindexPrev) { // pindexPrev is only null on the first block which is a version 1 block.
