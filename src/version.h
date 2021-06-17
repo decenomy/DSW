589cdef672f4e01776507f944d4091170f6199cf
perseu <jmsaguas@gmail.com>
2021-06-17 17:51:32 +0200

    changing version to match Soverain

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70100;
+static const int PROTOCOL_VERSION = 70013;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,7 +21,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70000;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70017;
+static const int MIN_PEER_MNANNOUNCE = 70003;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -34,7 +34,7 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
+static const int NO_BLOOM_VERSION = 70000;
 
 
 #endif // BITCOIN_VERSION_H
