457f50a741ab4625951f532ed2a8ee0c73044d78
pedro-at-decenomy <pedro@decenomy.net>
2021-07-22 03:19:37 +0100

    minor fix

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -383,9 +383,9 @@ CAmount CMasternode::GetBlockValue(int nHeight)
 
 CAmount CMasternode::GetMasternodePayment(int nHeight)
 {
-    if(nHeight > 1180000) return GetBlockValue(chainActive.Height()) * 65 / 100;
+    if(nHeight > 1180000) return GetBlockValue(nHeight) * 65 / 100;
 
-    return GetBlockValue(chainActive.Height()) * 95 / 100;
+    return GetBlockValue(nHeight) * 95 / 100;
 }
 
 void CMasternode::InitMasternodeCollateralList() {
