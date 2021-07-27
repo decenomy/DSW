01fc5badc8db77f332f2522e01b429c6f555ce54
Peteracn96 <pacn96@gmail.com>
2021-07-27 13:15:59 +0200

    Replace standard coin name __decenomy__ by oneworld

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -51,7 +51,7 @@ static const int MAX_URI_LENGTH = 255;
 
 
 #define QAPP_ORG_NAME "OneWorld"
-#define QAPP_ORG_DOMAIN "__decenomy__.org"
+#define QAPP_ORG_DOMAIN "oneworld.org"
 #define QAPP_APP_NAME_DEFAULT "OneWorld-Qt"
 #define QAPP_APP_NAME_TESTNET "OneWorld-Qt-testnet"
 
