9853c4aa42d1b737b02529d9d2be283c5a50c0b8
Peteracn96 <pacn96@gmail.com>
2021-07-27 16:05:51 +0200

    Change ports

--- src/chainparams.h
+++ src/chainparams.h
@@ -9,9 +9,9 @@
 #ifndef BITCOIN_CHAINPARAMS_H
 #define BITCOIN_CHAINPARAMS_H
 
-#define __PORT_MAINNET__ 9999
-#define __PORT_TESTNET__ 19999
-#define __PORT_REGTEST__ 29999
+#define __PORT_MAINNET__ 32112
+#define __PORT_TESTNET__ 51474
+#define __PORT_REGTEST__ 51476
 
 #include "chainparamsbase.h"
 #include "checkpoints.h"
