dd88ec3ef0ec7c9cf0cb46aba6e5d41ff24708c4
Peteracn96 <pacn96@gmail.com>
2021-07-29 17:39:15 +0200

    Set GUI colors

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -674,8 +674,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
 	set1 = new QBarSet("MN_" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(221,139,20));
-	set1->setColor(QColor(192,100,22));
+    set0->setColor(QColor(255,223,64));
+	set1->setColor(QColor(110,199,244));
 
     if (!series) {
         series = new QBarSeries();
