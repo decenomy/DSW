9a5c4db79fcce14e050983288abc6d22e79593fa
Bedri Özgür Güler <bedriguler@gmail.com>
2021-07-30 06:10:42 +0200

    Fixes some GUI color bugs

--- src/qt/locale/pivx_en.ts
+++ src/qt/locale/pivx_en.ts
@@ -4489,7 +4489,7 @@ There was an error trying to save the address list to %1. Please try again.</sou
         <source>
                            &lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;
                            We have support channels in most of our official chat groups, for example
-                           &lt;a style=&apos;color: #b088ff&apos; href=&apos;https://discord.gg/R2Racbr&apos;&gt;
+                           &lt;a style=&apos;color: #04CCCC&apos; href=&apos;https://discord.gg/R2Racbr&apos;&gt;
                            #support in our Discord&lt;/a&gt;.
                            &lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
                          </source>
