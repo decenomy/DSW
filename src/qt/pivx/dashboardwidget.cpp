187a8c3bb6d8fda436d69f445c6ddda7785d1301
stepollo2 <sbrasoinfinito@gmail.com>
2021-08-03 21:50:15 +0200

    additional color fixes

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -674,8 +674,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
 	set1 = new QBarSet("MN_" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(123,230,255));
-	set1->setColor(QColor(74,149,103));
+    set0->setColor(QColor(96, 252, 222));
+	set1->setColor(QColor(112, 217, 242));
 
     if (!series) {
         series = new QBarSeries();
