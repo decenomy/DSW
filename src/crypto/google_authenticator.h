cc2da0026236e21aff9625f4e67df26e1f042434
pedro-at-decenomy <pedro@decenomy.net>
2021-08-08 13:53:51 +0100

    implements GoogleAuthenticator::GeneratePin

--- src/crypto/google_authenticator.h
+++ src/crypto/google_authenticator.h
@@ -5,15 +5,22 @@
 #ifndef DECENOMY_CRYPTO_GOOGLE_AUTHENTICATOR_H
 #define DECENOMY_CRYPTO_GOOGLE_AUTHENTICATOR_H
 
-#include "crypto/hmac_sha1.h"
+#include <cstdint>
 
 /** A Google Authenticator support class. */
 class GoogleAuthenticator
 {
 private:
-    
+    static const int INTERVAL_LENGHT = 30;
+    static const int PIN_LENGHT = 6;
+
+    const unsigned char* key;
+    const int len;
 public:
-    
+    GoogleAuthenticator(const unsigned char* key, const int len) : 
+        key(key), len(len) {}
+
+    int GeneratePin();
 };
 
 #endif // DECENOMY_CRYPTO_GOOGLE_AUTHENTICATOR_H
