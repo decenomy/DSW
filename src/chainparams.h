3d75492434d411569982b8a77c48bcef2417ae9b
perseu <jmsaguas@gmail.com>
2021-09-15 12:58:23 +0200

    Setting default ports.

--- src/chainparams.h
+++ src/chainparams.h
@@ -9,9 +9,9 @@
 #ifndef BITCOIN_CHAINPARAMS_H
 #define BITCOIN_CHAINPARAMS_H
 
-#define __PORT_MAINNET__ 9999
-#define __PORT_TESTNET__ 19999
-#define __PORT_REGTEST__ 29999
+#define __PORT_MAINNET__ 15252
+#define __PORT_TESTNET__ 7777
+#define __PORT_REGTEST__ 51436
 
 #include "chainparamsbase.h"
 #include "checkpoints.h"
