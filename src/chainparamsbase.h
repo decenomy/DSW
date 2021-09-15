06a3e95120c6b41e5aba322127b882f88189afe9
Peteracn96 <pacn96@gmail.com>
2021-09-15 16:47:28 +0200

    Replace standard coin name __decenomy__ by peps (1)

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -14,8 +14,8 @@
 #include <vector>
 
 /**
- * CBaseChainParams defines the base parameters (shared between __decenomy__-cli and __decenomy__d)
- * of a given instance of the __decenomy__ system.
+ * CBaseChainParams defines the base parameters (shared between peps-cli and pepsd)
+ * of a given instance of the peps system.
  */
 class CBaseChainParams
 {
--- src/init.cpp
+++ src/init.cpp
@@ -927,7 +927,7 @@ void InitLogging()
     LogPrintf("Peps version %s (%s)\n", version_string, CLIENT_DATE);
 }
 
-/** Initialize __decenomy__.
+/** Initialize peps.
  *  @pre Parameters should be parsed and config file should be read.
  */
 bool AppInit2()
