174623eb9c127395485a1313deb8ebc2b40ef881
pedro-at-decenomy <pedro@decenomy.net>
2021-09-16 12:16:07 +0100

    fchanges the XEVAN hash function in a non-standard way to fit the original modifed XEVAN function

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -17,12 +17,22 @@
 // TODO: Change X11KVS algorithm call to whatever the coin being adapted is used.
 uint256 CBlockHeader::GetHash() const
 {
-
-    if(nVersion < 4)
+    if (nVersion < 4)  {
+#if defined(WORDS_BIGENDIAN)
+        uint8_t data[80];
+        WriteLE32(&data[0], nVersion);
+        memcpy(&data[4], hashPrevBlock.begin(), hashPrevBlock.size());
+        memcpy(&data[36], hashMerkleRoot.begin(), hashMerkleRoot.size());
+        WriteLE32(&data[68], nTime);
+        WriteLE32(&data[72], nBits);
+        WriteLE32(&data[76], nNonce);
+        return XEVAN(data, data + 80);
+#else // Can take shortcut for little endian
         return XEVAN(BEGIN(nVersion), END(nNonce));
-
-    return Hash(BEGIN(nVersion), END(nAccumulatorCheckpoint));
-
+#endif
+    }
+    // version >= 4
+    return SerializeHash(*this);
 }
 
 std::string CBlock::ToString() const
