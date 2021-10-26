bfdb27b8206a51a7f1218a16ab9f44e1715295bd
Peteracn96 <pacn96@gmail.com>
2021-10-26 18:07:41 +0200

    Replace standard coin unit __DSW__ by MOBIC (3)

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -350,7 +350,7 @@ void AskPassphraseDialog::warningMessage()
             "<qt>" +
             tr("__Decenomy__ will close now to finish the encryption process. "
                "Remember that encrypting your wallet cannot fully protect "
-               "your __DSW__s from being stolen by malware infecting your computer.") +
+               "your MOBICs from being stolen by malware infecting your computer.") +
             "<br><br><b>" +
             tr("IMPORTANT: Any previous backups you have made of your wallet file "
                "should be replaced with the newly generated, encrypted wallet file. "
