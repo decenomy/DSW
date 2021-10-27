0ebb784423ff3868bd6fee6a9c39d33c75c59cfb
Peteracn96 <pacn96@gmail.com>
2021-10-27 17:22:57 +0200

    Replace standard coin name __Decenomy__ by MobilityCoin (4)

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -266,11 +266,11 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.fHelp || jsonRequest.params.size() > 1)
         throw std::runtime_error(
             "stop\n"
-            "\nStop __Decenomy__ server.");
+            "\nStop MobilityCoin server.");
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
-    return "__Decenomy__ server stopping";
+    return "MobilityCoin server stopping";
 }
 
 
