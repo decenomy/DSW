4811e941a7e778227775015ae07dee4a8283669f
Peteracn96 <pacn96@gmail.com>
2021-10-28 16:12:52 +0200

    Replace standard coin name __decenomy__ by mobic (4)

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -361,31 +361,31 @@ static const CRPCCommand vRPCCommands[] =
         { "hidden",             "waitforblockheight",     &waitforblockheight,     true },
 
         /* MOBIC features */
-        {"__decenomy__", "listmasternodes", &listmasternodes, true },
-        {"__decenomy__", "getmasternodecount", &getmasternodecount, true },
-        {"__decenomy__", "createmasternodebroadcast", &createmasternodebroadcast, true },
-        {"__decenomy__", "decodemasternodebroadcast", &decodemasternodebroadcast, true },
-        {"__decenomy__", "relaymasternodebroadcast", &relaymasternodebroadcast, true },
-        {"__decenomy__", "masternodecurrent", &masternodecurrent, true },
-        {"__decenomy__", "startmasternode", &startmasternode, true },
-        {"__decenomy__", "createmasternodekey", &createmasternodekey, true },
-        {"__decenomy__", "getmasternodeoutputs", &getmasternodeoutputs, true },
-        {"__decenomy__", "listmasternodeconf", &listmasternodeconf, true },
-        {"__decenomy__", "getmasternodestatus", &getmasternodestatus, true },
-        {"__decenomy__", "getmasternodewinners", &getmasternodewinners, true },
-        {"__decenomy__", "getmasternodescores", &getmasternodescores, true },
-        {"__decenomy__", "preparebudget", &preparebudget, true },
-        {"__decenomy__", "submitbudget", &submitbudget, true },
-        {"__decenomy__", "mnbudgetvote", &mnbudgetvote, true },
-        {"__decenomy__", "getbudgetvotes", &getbudgetvotes, true },
-        {"__decenomy__", "getnextsuperblock", &getnextsuperblock, true },
-        {"__decenomy__", "getbudgetprojection", &getbudgetprojection, true },
-        {"__decenomy__", "getbudgetinfo", &getbudgetinfo, true },
-        {"__decenomy__", "mnbudgetrawvote", &mnbudgetrawvote, true },
-        {"__decenomy__", "mnfinalbudget", &mnfinalbudget, true },
-        {"__decenomy__", "checkbudgets", &checkbudgets, true },
-        {"__decenomy__", "mnsync", &mnsync, true },
-        {"__decenomy__", "spork", &spork, true },
+        {"mobic", "listmasternodes", &listmasternodes, true },
+        {"mobic", "getmasternodecount", &getmasternodecount, true },
+        {"mobic", "createmasternodebroadcast", &createmasternodebroadcast, true },
+        {"mobic", "decodemasternodebroadcast", &decodemasternodebroadcast, true },
+        {"mobic", "relaymasternodebroadcast", &relaymasternodebroadcast, true },
+        {"mobic", "masternodecurrent", &masternodecurrent, true },
+        {"mobic", "startmasternode", &startmasternode, true },
+        {"mobic", "createmasternodekey", &createmasternodekey, true },
+        {"mobic", "getmasternodeoutputs", &getmasternodeoutputs, true },
+        {"mobic", "listmasternodeconf", &listmasternodeconf, true },
+        {"mobic", "getmasternodestatus", &getmasternodestatus, true },
+        {"mobic", "getmasternodewinners", &getmasternodewinners, true },
+        {"mobic", "getmasternodescores", &getmasternodescores, true },
+        {"mobic", "preparebudget", &preparebudget, true },
+        {"mobic", "submitbudget", &submitbudget, true },
+        {"mobic", "mnbudgetvote", &mnbudgetvote, true },
+        {"mobic", "getbudgetvotes", &getbudgetvotes, true },
+        {"mobic", "getnextsuperblock", &getnextsuperblock, true },
+        {"mobic", "getbudgetprojection", &getbudgetprojection, true },
+        {"mobic", "getbudgetinfo", &getbudgetinfo, true },
+        {"mobic", "mnbudgetrawvote", &mnbudgetrawvote, true },
+        {"mobic", "mnfinalbudget", &mnfinalbudget, true },
+        {"mobic", "checkbudgets", &checkbudgets, true },
+        {"mobic", "mnsync", &mnsync, true },
+        {"mobic", "spork", &spork, true },
 
 #ifdef ENABLE_WALLET
         /* Wallet */
@@ -604,7 +604,7 @@ std::vector<std::string> CRPCTable::listCommands() const
 
 std::string HelpExampleCli(std::string methodname, std::string args)
 {
-    return "> __decenomy__-cli " + methodname + " " + args + "\n";
+    return "> mobic-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(std::string methodname, std::string args)
