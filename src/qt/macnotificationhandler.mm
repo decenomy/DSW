1b0567596db7a418dc5db0b824b5b48fc829d62f
Peteracn96 <pacn96@gmail.com>
2021-10-28 18:01:27 +0200

    Replace standard coin name __decenomy__ by mobic (10)

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -15,7 +15,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.__decenomy__.MobilityCoin-Qt";
+        return @"io.mobic.MobilityCoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
