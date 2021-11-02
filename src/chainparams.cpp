772ba80c99b6f5006e8267973b29d24a7837a5fa
Peteracn96 <pacn96@gmail.com>
2021-11-02 14:01:46 +0100

    Change message starts

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -238,10 +238,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0xc8;
-        pchMessageStart[1] = 0x7b;
-        pchMessageStart[2] = 0x86;
-        pchMessageStart[3] = 0x77;
+        pchMessageStart[0] = 0x6c;
+        pchMessageStart[1] = 0x79;
+        pchMessageStart[2] = 0x6e;
+        pchMessageStart[3] = 0x76;
         nDefaultPort = __PORT_MAINNET__;
 
         vSeeds.push_back(CDNSSeedData("seeder", "seeder.__decenomy.net__"));
