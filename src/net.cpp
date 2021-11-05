689017961425c6a97aa8d41b928a1a55a2c96116
pedro-at-decenomy <sapphire_pt@sappcoin.com>
2021-11-05 11:09:15 +0000

    only disconnects if the first connection eviction fails

--- src/net.cpp
+++ src/net.cpp
@@ -1099,15 +1099,19 @@ void CConnman::AcceptConnection(const ListenSocket& hListenSocket) {
 
     if (nInbound >= nMaxConnections - MAX_OUTBOUND_CONNECTIONS) {
         // try to evict 10% of the inbound connections
-        int n = std::max(1, (nMaxConnections - MAX_OUTBOUND_CONNECTIONS) / 10); 
+        int n = std::max(1, (nMaxConnections - MAX_OUTBOUND_CONNECTIONS) / 10);
+        int evicted = 0;
         for(int i = 0; i < n; i++) {
-            if (!AttemptToEvictConnection(whitelisted)) {
+            if (!AttemptToEvictConnection(whitelisted) && evicted == 0) {
                 // No connection to evict, disconnect the new connection
                 LogPrint(BCLog::NET, "failed to find an eviction candidate - connection dropped (full)\n");
                 CloseSocket(hSocket);
                 return;
+            } else {
+                evicted++;
             }
         }
+        LogPrint(BCLog::NET, "%d connections evicted\n", evicted);
     }
 
     NodeId id = GetNewNodeId();
