b49acbcaba841a86fe2f81a48351a61f68f71053
Peteracn96 <pacn96@gmail.com>
2021-11-09 17:27:57 +0100

    Replace test net port placeholders

--- contrib/seeds/generate-seeds.py
+++ contrib/seeds/generate-seeds.py
@@ -131,7 +131,7 @@ def main():
         process_nodes(g, f, 'pnSeed6_main', 8822)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'), 'r', encoding="utf8") as f:
-        process_nodes(g, f, 'pnSeed6_test', __PORT_TESTNET__)
+        process_nodes(g, f, 'pnSeed6_test', 59999)
     g.write('#endif // BITCOIN_CHAINPARAMSSEEDS_H\n')
 
 if __name__ == '__main__':
--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -372,7 +372,7 @@ public:
         pchMessageStart[1] = 0x76;
         pchMessageStart[2] = 0x65;
         pchMessageStart[3] = 0xba;
-        nDefaultPort = __PORT_TESTNET__;
+        nDefaultPort = 59999;
 
         vFixedSeeds.clear();
         vSeeds.clear();
