63bb61de0d15892d4af2518fd7a88d391575ccf4
pedro-at-decenomy <sapphire_pt@sappcoin.com>
2021-11-29 16:11:53 +0000

    fixes the BIR secret key base58 prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -321,7 +321,7 @@ public:
         base58Prefixes[EXT_COIN_TYPE]   = boost::assign::list_of(0x80)(0x00)(0x03)(0x46).convert_to_container<std::vector<unsigned char> >();
 
         base58Prefixes[PUBKEY_ADDRESS_BIR]  = std::vector<unsigned char>(1, 45);
-        base58Prefixes[SECRET_KEY_BIR]      = std::vector<unsigned char>(1, 139);        
+        base58Prefixes[SECRET_KEY_BIR]      = std::vector<unsigned char>(1, 179);        
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
         //convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main)); // added
