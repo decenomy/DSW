d9a7efae9f24c3fbf606894da26e1e30dc974cb4
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2022-01-12 15:04:38 +0100

    standard maxmoneyout

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -167,7 +167,7 @@ public:
         consensus.nCoinbaseMaturity = 100;
         consensus.nFutureTimeDriftPoW = 7200;
         consensus.nFutureTimeDriftPoS = 180;
-        consensus.nMaxMoneyOut = 500000000 * COIN;
+        consensus.nMaxMoneyOut = 9999999999 * COIN;
         consensus.nPoolMaxTransactions = 3;
         consensus.nProposalEstablishmentTime = 60 * 60 * 24;    // must be at least a day old to make it into a budget
         consensus.nStakeMinAge = 60 * 60; // 1h
