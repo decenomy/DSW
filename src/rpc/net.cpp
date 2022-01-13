2dea5caf5256b157adb89db280580e5475178c4a
Peteracn96 <pacn96@gmail.com>
2022-01-13 18:57:18 +0100

    Replace main net port placeholder

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -261,7 +261,7 @@ UniValue getaddednodeinfo(const JSONRPCRequest& request)
             "    \"connected\" : true|false,          (boolean) If connected\n"
             "    \"addresses\" : [                    (list of objects) Only when connected = true\n"
             "       {\n"
-            "         \"address\" : \"192.168.0.201:__PORT_MAINNET__\",  (string) The mobic server IP and port we're connected to\n"
+            "         \"address\" : \"192.168.0.201:22487\",  (string) The mobic server IP and port we're connected to\n"
             "         \"connected\" : \"outbound\"           (string) connection, inbound or outbound\n"
             "       }\n"
             "     ]\n"
