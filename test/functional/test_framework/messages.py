96a3d6ac35284f6e245e446b439854afafb85237
Peteracn96 <pacn96@gmail.com>
2022-04-26 16:48:47 +0200

    Replace standard coin name __decenomy__ by flits (6)

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -503,7 +503,7 @@ class CBlockHeader():
         self.calc_sha256()
         return self.sha256
 
-    # __decenomy__
+    # flits
     def solve_stake(self, stakeInputs, prevModifier):
         target0 = uint256_from_compact(self.nBits)
         loop = True
