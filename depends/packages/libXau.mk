2b625d2b3be65a299cc52dab74e0ba551e3e1087
stepollo2 <sbrasoinfinito@gmail.com>
2022-06-15 13:35:34 +0200

    change depends  links

--- depends/packages/libXau.mk
+++ depends/packages/libXau.mk
@@ -1,7 +1,7 @@
 package=libXau
 $(package)_version=1.0.8
 # $(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
-$(package)_download_path=https://cloudflare-ipfs.com/ipfs/QmYv9wHqKa5gZE1kL4ZpcVqKp1coS5tS9cYKGe4mUxxrqL
+$(package)_download_path=https://github.com/decenomy/depends/raw/main/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2
 $(package)_dependencies=xproto
