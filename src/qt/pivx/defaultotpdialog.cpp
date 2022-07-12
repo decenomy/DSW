d1933ae7b72b99d3df38cfcc0f2b5bc621a86258
Liquid <Liquid369@gmail.com>
2022-07-12 07:54:32 -0500

    [Qt][Build] Add base forms

---
+++ src/qt/pivx/defaultotpdialog.cpp
@@ -0,0 +1,79 @@
+// Copyright (c) 2019-2020 The PIVX developers
+// Copyright (c) 2021-2022 The DECENOMY Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "qt/pivx/defaultotpdialog.h"
+#include "qt/pivx/qtutils.h"
+#include "qt/pivx/forms/ui_defaultotpdialog.h"
+#include "guiutil.h"
+
+#include <QPixmap>
+#include <QString>
+
+DefaultOtpDialog::DefaultOtpDialog(QWidget *parent) :
+    FocusedDialog(parent),
+    ui(new Ui::DefaultOtpDialog)
+{
+    ui->setupUi(this);
+
+    // Stylesheet
+    this->setStyleSheet(parent ? parent->styleSheet() : GUIUtil::loadStyleSheet());
+
+    // Container
+    ui->frame->setProperty("cssClass", "container-dialog");
+
+    // Text
+    ui->labelTitle->setProperty("cssClass", "text-title-dialog");
+    ui->labelMessage->setProperty("cssClass", "text-main-grey");
+
+    // QrCode
+    ui->labelOtp->setText("");
+
+    // Buttons
+    ui->btnEsc->setText("");
+    ui->btnEsc->setProperty("cssClass", "ic-close");
+
+    ui->btnCancel->setProperty("cssClass", "btn-dialog-cancel");
+    ui->btnSave->setProperty("cssClass", "btn-primary");
+
+    connect(ui->btnEsc, &QPushButton::clicked, this, &DefaultOtpDialog::close);
+    connect(ui->btnCancel, &QPushButton::clicked, this, &DefaultOtpDialog::close);
+    connect(ui->btnSave, &QPushButton::clicked, this, &DefaultOtpDialog::accept);
+}
+
+void DefaultOtpDialog::setText(const QString& title, const QString& message, const QString& okBtnText, const QString& cancelBtnText)
+{
+    if (!okBtnText.isNull()) ui->btnSave->setText(okBtnText);
+    if (!cancelBtnText.isNull()) {
+        ui->btnCancel->setVisible(true);
+        ui->btnCancel->setText(cancelBtnText);
+    } else {
+        ui->btnCancel->setVisible(false);
+    }
+    if (!message.isNull()) ui->labelMessage->setText(message);
+    if (!title.isNull()) ui->labelTitle->setText(title);
+}
+
+void DefaultOtpDialog::setQrCode(QString qrStr)
+{
+    QString error;
+    QPixmap otpQr = encodeToQr(qrStr, error);
+    if (!otpQr.isNull()) {
+        qrImage = &otpQr;
+        ui->labelOtp->setPixmap(qrImage->scaled(ui->labelOtp->width(), ui->labelOtp->height()));
+    } else{
+        ui->labelOtp->setText(!error.isEmpty() ? error : "Error encoding Seedphrase.");
+    }
+}
+
+void DefaultOtpDialog::accept()
+{
+    this->isOk = true;
+    QDialog::accept();
+}
+
+DefaultOtpDialog::~DefaultDialog()
+{
+    delete ui;
+}
---
+++ src/qt/pivx/defaultotpdialog.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2019-2020 The PIVX developers
+// Copyright (c) 2021-2022 The DECENOMY Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DEFAULTOTPDIALOG_H
+#define DEFAULTOTPDIALOG_H
+
+#include "qt/pivx/focuseddialog.h"
+#include <QPixmap>
+
+namespace Ui {
+class DefaultOtpDialog;
+}
+
+class DefaultOtpDialog : public FocusedDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DefaultOtpDialog(QWidget *parent = nullptr);
+    ~DefaultOtpDialog();
+
+    void setText(const QString& title = "", const QString& message = "", const QString& okBtnText = "", const QString& cancelBtnText = "");
+    void setQrCode(QString qrStr);
+
+    bool isOk = false;
+
+public Q_SLOTS:
+    void accept() override;
+
+private:
+    Ui::DefaultOtpDialog *ui;
+    QPixmap *qrImage;
+};
+
+#endif // DEFAULTDIALOG_H
