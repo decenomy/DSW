9a569ad27254060a9835340badc19333ea30f9a0
pedro-at-decenomy <pedro@decenomy.net>
2022-11-08 14:31:00 +0000

    shows only non-NOOP sporks

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -266,7 +266,9 @@ UniValue spork(const JSONRPCRequest& request)
     if (request.params.size() == 1 && request.params[0].get_str() == "show") {
         UniValue ret(UniValue::VOBJ);
         for (const auto& sporkDef : sporkDefs) {
-            ret.push_back(Pair(sporkDef.name, sporkManager.GetSporkValue(sporkDef.sporkId)));
+            if (sporkDef.name.find("NOOP") == std::string::npos) { // show only useful sporks
+                ret.push_back(Pair(sporkDef.name, sporkManager.GetSporkValue(sporkDef.sporkId)));
+            }
         }
         return ret;
     } else if (request.params.size() == 1 && request.params[0].get_str() == "active") {
