b8cd1038e65d59e837de73b653141ba0551e6e19
pedro-at-decenomy <pedro@decenomy.net>
2022-12-29 13:23:15 +0000

    close lagging nodes' connection

--- src/net.cpp
+++ src/net.cpp
@@ -1068,6 +1068,21 @@ void CConnman::AcceptConnection(const ListenSocket& hListenSocket) {
     int nMaxInbound = nMaxConnections - (nMaxOutbound + nMaxFeeler);
     assert(nMaxInbound > 0);
 
+    // close lagging nodes' connection
+    {
+        LOCK(cs_vNodes);
+        for (CNode* pnode : vNodes) {
+            int64_t nPingUsecWait = 0;
+            if ((pnode->nPingNonceSent != 0) && (pnode->nPingUsecStart != 0)) {
+                nPingUsecWait = GetTimeMicros() - pnode->nPingUsecStart;
+            }
+            if (pnode->nPingUsecTime > 5 * 1e6 || nPingUsecWait > 5 * 1e6) {
+                pnode->fDisconnect = true;
+                pnode->CloseSocketDisconnect();
+            }
+        }
+    }
+
     if (hSocket != INVALID_SOCKET)
         if (!addr.SetSockAddr((const struct sockaddr*)&sockaddr))
             LogPrintf("Warning: Unknown socket family\n");
@@ -1076,7 +1091,7 @@ void CConnman::AcceptConnection(const ListenSocket& hListenSocket) {
     {
         LOCK(cs_vNodes);
         for (CNode* pnode : vNodes)
-            if (pnode->fInbound)
+            if (pnode->fInbound && !pnode->fDisconnect)
                 nInbound++;
     }
 
