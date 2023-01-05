47465faa8cbdd26e127bf4b107ca8c76aa3276a8
Caprica01 <antoninimichele8@gmail.com>
2023-01-05 20:15:48 +0100

    clean merge problem

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -219,7 +219,6 @@ public:
         consensus.nFutureTimeDriftPoS = 180;
         consensus.nMaxMoneyOut = 9999999999 * COIN;
         consensus.nPoolMaxTransactions = 3;
-        consensus.nProposalEstablishmentTime = 60 * 60 * 24;    // must be at least a day old to make it into a budget
         consensus.nStakeMinAge = 60 * 60; // 1h
         consensus.nStakeMinDepth = 100;
         consensus.nStakeMinDepthV2 = 600;
@@ -240,38 +239,12 @@ public:
             { "cBurnAddressXXXXXXXXXXXXXXXXVrq7yw", 0 },
         };
 
-        // height-based activations
-        consensus.height_last_ZC_AccumCheckpoint = DISABLED;
-        consensus.height_last_ZC_WrappedSerials = DISABLED;
-        consensus.height_start_InvalidUTXOsCheck = DISABLED;
-        consensus.height_start_ZC_InvalidSerials = DISABLED;
-        consensus.height_start_ZC_SerialRangeCheck = DISABLED;
-        consensus.height_ZC_RecalcAccumulators = DISABLED;
-
-        // Zerocoin-related params
-        consensus.ZC_Modulus = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784"
-                               "4069182906412495150821892985591491761845028084891200728449926873928072877767359714183472702618963750149718246911"
-                               "6507761337985909570009733045974880842840179742910064245869181719511874612151517265463228221686998754918242243363"
-                               "7259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133"
-                               "8441436038339044149526344321901146575444541784240209246165157233507787077498171257724679629263863563732899121548"
-                               "31438167899885040445364023527381951378636564391212010397122822120720357";
-        consensus.ZC_MaxPublicSpendsPerTx = 637; // Assume about 220 bytes each input
-        consensus.ZC_MaxSpendsPerTx = 7;         // Assume about 20kb each input
-        consensus.ZC_MinMintConfirmations = 20;
-        consensus.ZC_MinMintFee = 1 * CENT;
-        consensus.ZC_MinStakeDepth = 200;
-        consensus.ZC_TimeStart = 1893456000;   // 01/01/2030 @ 12:00am (UTC)
-        consensus.ZC_WrappedSerialsSupply = 0; //4131563 * COIN;   // zerocoin supply at height_last_ZC_WrappedSerials
-
         // Network upgrades
         consensus.vUpgrades[Consensus::BASE_NETWORK].nActivationHeight                      = Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
         consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight                 = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
         consensus.vUpgrades[Consensus::UPGRADE_POS].nActivationHeight                       = 1001;
         consensus.vUpgrades[Consensus::UPGRADE_POS_V2].nActivationHeight                    = 1441;
-        consensus.vUpgrades[Consensus::UPGRADE_ZC].nActivationHeight                        = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-        consensus.vUpgrades[Consensus::UPGRADE_ZC_V2].nActivationHeight                     = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
         consensus.vUpgrades[Consensus::UPGRADE_BIP65].nActivationHeight                     = 1441;
-        consensus.vUpgrades[Consensus::UPGRADE_ZC_PUBLIC].nActivationHeight                 = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
         consensus.vUpgrades[Consensus::UPGRADE_STAKE_MODIFIER_V2].nActivationHeight         = 1541;
         consensus.vUpgrades[Consensus::UPGRADE_TIME_PROTOCOL_V2].nActivationHeight          = 1641;
         consensus.vUpgrades[Consensus::UPGRADE_P2PKH_BLOCK_SIGNATURES].nActivationHeight    = 1741;
