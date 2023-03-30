196703467d466e3ec5db5a347354a90e720d2a51
pedro-at-decenomy <pedro@decenomy.net>
2023-03-30 21:43:47 +0100

    adds EBF upgrade consensus activation

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -197,6 +197,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_P2PKH_BLOCK_SIGNATURES].nActivationHeight = 1741;
         consensus.vUpgrades[Consensus::UPGRADE_STAKE_MIN_DEPTH_V2].nActivationHeight     = 5001;
         consensus.vUpgrades[Consensus::UPGRADE_MASTERNODE_RANK_V2].nActivationHeight     = 5001;
+        consensus.vUpgrades[Consensus::UPGRADE_EBF].nActivationHeight                    = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;;
 
         consensus.vUpgrades[Consensus::UPGRADE_POS].hashActivationBlock                    = uint256S("0x0");
         consensus.vUpgrades[Consensus::UPGRADE_POS_V2].hashActivationBlock                 = uint256S("0x0");
@@ -206,6 +207,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_P2PKH_BLOCK_SIGNATURES].hashActivationBlock = uint256S("0x0");
         consensus.vUpgrades[Consensus::UPGRADE_STAKE_MIN_DEPTH_V2].hashActivationBlock     = uint256S("0x0");
         consensus.vUpgrades[Consensus::UPGRADE_MASTERNODE_RANK_V2].hashActivationBlock     = uint256S("0x0");
+        consensus.vUpgrades[Consensus::UPGRADE_EBF].hashActivationBlock                    = uint256S("0x0");
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
