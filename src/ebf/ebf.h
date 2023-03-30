ab2fb629f58990e154e21f57191340b37a79d781
pedro-at-decenomy <pedro@decenomy.net>
2023-03-30 21:27:48 +0100

    adds new EBF code files

---
+++ src/ebf/ebf.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2023 The DECENOMY Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DECENOMY_EBF_H
+#define DECENOMY_EBF_H
+
+enum EbfPlatforms { // 0-31 (32 Slots)
+    BASE        = 0x00,
+    JACKPOT     = 0x01,
+};
+
+enum EbfType { // 0-7 (8 Slots for one byte, more are possible by using varint) 
+    // System
+    HODL_CONTRACT           = 0x00 << 5 + EbfPlatforms::BASE,
+    MASTERNODE_CONTRACT     = 0x01 << 5 + EbfPlatforms::BASE,
+    
+    // Jackpot
+    DICE_CONTRACT           = 0x00 << 5 + EbfPlatforms::JACKPOT,
+    ROULETTE_CONTRACT       = 0x01 << 5 + EbfPlatforms::JACKPOT,
+    LOTTERY_CONTRACT        = 0X02 << 5 + EbfPlatforms::JACKPOT,
+};
+
+#endif // DECENOMY_EBF_H
