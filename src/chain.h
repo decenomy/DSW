10d1a1fae1b368b31e7859c4efce7611022e72bc
pedro-at-decenomy <pedro@decenomy.net>
2023-06-26 11:50:12 +0100

    activates DBI_SER_VERSION_MS

--- src/chain.h
+++ src/chain.h
@@ -283,7 +283,7 @@ public:
 // New serialization introduced on PIVX
 static const int DBI_SER_VERSION_NO_MS = 0;   // removes nMoneySupply from persisted block index
 // New serialization introduced on DSW
-static const int DBI_SER_VERSION_MS = INT32_MAX;   // reintroduces the nMoneySupply to the persisted block index
+static const int DBI_SER_VERSION_MS = 4010000;   // reintroduces the nMoneySupply to the persisted block index
 
 class CDiskBlockIndex : public CBlockIndex
 {
