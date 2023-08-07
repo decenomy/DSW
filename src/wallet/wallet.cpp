0edee622271e43517c3967440254178fc5f848b5
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2023-08-07 21:33:32 +0200

    Distinguish between old and new wallets in AutoCombineDust

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3242,7 +3242,13 @@ void CWallet::AutoCombineDust(CConnman* connman)
         return;
     }
 
-    std::map<CTxDestination, std::vector<COutput> > mapCoinsByAddress = AvailableCoinsByAddress(true, nAutoCombineThreshold * COIN);
+    // Make sure we don't break the settings saved in the old wallets where the autocombine threshold was saved incorrectly.
+    CAmount adjustedAutoCombineThreshold = nAutoCombineThreshold;
+    if (nAutoCombineThreshold < COIN) {
+        adjustedAutoCombineThreshold = nAutoCombineThreshold * COIN;
+    }
+
+    std::map<CTxDestination, std::vector<COutput> > mapCoinsByAddress = AvailableCoinsByAddress(true, adjustedAutoCombineThreshold);
 
     //coins are sectioned by address. This combination code only wants to combine inputs that belong to the same address
     for (std::map<CTxDestination, std::vector<COutput> >::iterator it = mapCoinsByAddress.begin(); it != mapCoinsByAddress.end(); it++) {
@@ -3270,7 +3276,7 @@ void CWallet::AutoCombineDust(CConnman* connman)
             nTotalRewardsValue += out.Value();
 
             // Combine to the threshold and not way above
-            if (nTotalRewardsValue > nAutoCombineThreshold * COIN)
+            if (nTotalRewardsValue > adjustedAutoCombineThreshold)
                 break;
 
             // Around 180 bytes per input. We use 190 to be certain
@@ -3317,7 +3323,7 @@ void CWallet::AutoCombineDust(CConnman* connman)
         }
 
         //we don't combine below the threshold unless the fees are 0 to avoid paying fees over fees over fees
-        if (!maxSize && nTotalRewardsValue < nAutoCombineThreshold * COIN && nFeeRet > 0)
+        if (!maxSize && nTotalRewardsValue < adjustedAutoCombineThreshold && nFeeRet > 0)
             continue;
 
         const CWallet::CommitResult& res = CommitTransaction(wtx, keyChange, connman);
