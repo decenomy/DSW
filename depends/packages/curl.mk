89d5eb5e610d3017fa84b4ca0a47bff1ed8a8a3a
zimbora <lucas.ua.eet@gmail.com>
2023-11-24 00:36:18 +0000

    changes to a release with config script enabled

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -1,12 +1,13 @@
 package=curl
-$(package)_version=8_4_0
-$(package)_download_path=https://github.com/curl/curl/archive/refs/tags/
+$(package)_version=8.4.0
+$(package)_download_path=https://github.com/curl/curl/releases/download/curl-8_4_0/
 #$(package)_download_path=https://github.com/decenomy/depends/raw/main/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=5f7097e27b00e54d5522d80c0d44899084a226e9372a0d5d799ad3125dd4b8c0
+$(package)_sha256_hash=816e41809c043ff285e8c0f06a75a1fa250211bbfb2dc0a037eeef39f1a9e427
+$(package)_dependencies=openssl
 
 define $(package)_set_vars
-  $(package)_config_opts=--prefix=$($(package)_staging_dir)
+  $(package)_config_opts=--prefix=$($(package)_staging_dir) --with-openssl
 endef
 
 define $(package)_config_cmds
