d956b26628f6be0dcebc3d6df89fd50ee13417b1
lucas <lucas.ua.eet@gmail.com>
2024-01-04 01:07:23 +0000

    removes debian package, and creates it dynamically

--- contrib/debian/Readme.md
+++ contrib/debian/Readme.md
@@ -1,13 +1,18 @@
-# Files to build deb package
-
-## Before a new release
-  !! Edit changelog file
+# Script to build deb package
 
 ## Dependencies
 >> sudo apt-get install debhelper
 
-## Build deb package
+## Setup
   Go to parent directory
   Cp binary file __decenomy__-qt binary to this folder
+  Cp bitcoin.png image for this folder
 
->> dpkg-buildpackage -us -uc -b
+## Build
+
+### Build (no args)
+>> ./build.sh
+### Build (request help)
+>> ./build.sh --help
+### Build (example)
+>> ./build.sh --ticker 777 --package wallet --version 3.0.0.1 --notes "amazing app"
---
+++ contrib/debian/deb_build.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Default value for TICKER
+TICKER="__DSW__"
+PACKAGE="wallet"
+VERSION="3.0.0.0"
+NOTES="New release."
+
+name="zimbora"
+email="lucas.ua.eet@gmail.com"
+LC_TIME=en_US.UTF-8
+formatted_date=$(date -u "+%a, %d %b %Y %H:%M:%S +0000")
+
+# Function to check if a version is in the format x.y.z.u
+is_valid_version() {
+  local version="$1"
+  if [[ "$version" =~ ^[0-99]+\.[0-99]+\.[0-99]+\.[0-99]+$ ]]; then
+    return 0  # Valid version format
+  else
+    return 1  # Invalid version format
+  fi
+}
+
+# Parse command line options
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --help)
+      echo "
+        --ticker: set ticker
+        --package: package name
+        --version: set version
+        --notes: set changelog notes
+      "
+      exit 1
+      ;;
+    --ticker)
+      TICKER="$2"
+      shift 2
+      ;;
+    --package)
+      PACKAGE="$2"
+      # Check if the PACKAGE string contains only lowercase letters
+      if ! [[ "$PACKAGE" =~ ^[a-z]+$ ]]; then
+          echo "The package args does not contain only lowercase letters."
+          exit 1
+      fi
+      shift 2
+      ;;
+    --version)
+      VERSION="$2"
+      # Check if the provided version is in the format x.y.z.u
+      if ! is_valid_version "$VERSION"; then
+        echo "Invalid version format. Please use x.y.z.u format."
+        exit 1
+      fi
+      shift 2
+      ;;
+    --notes)
+      NOTES="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+echo "ticker: $TICKER"
+echo "package: $PACKAGE"
+echo "version: $VERSION"
+
+#make dirs
+rm -r debian
+mkdir debian
+
+#create changelog file
+echo "${PACKAGE} (${VERSION}) stable; urgency=medium
+
+ * ${NOTES}
+
+ -- ${name} <${email}>  ${formatted_date}" >> debian/changelog
+
+#create compat
+echo "14" >> debian/compat
+
+#create control
+echo "Source: ${PACKAGE}
+Section: misc
+Priority: optional
+Maintainer: ${name} <${email}>
+Build-Depends: debhelper (>= 9)
+
+Package: ${PACKAGE}
+Architecture: any
+Depends:
+Description: ${TICKER} wallet
+Homepage: https://explorer.decenomy.net/
+" >> debian/control
+
+#create install
+echo "debian/app.desktop usr/share/applications/" >> debian/install
+
+#create rules
+echo "#!/usr/bin/make -f
+
+export TICKER=${TICKER}
+
+clean:
+	@# Do nothing
+
+build:
+	@# Do nothing
+
+binary:
+	echo "${TICKER}"
+	mkdir -p debian/wallet/usr/bin
+	cp __decenomy__-qt debian/wallet/usr/bin/"${TICKER}"
+	mkdir -p debian/wallet/usr/share/applications
+	cp bitcoin.png debian/wallet/usr/share/applications
+	echo \"[Desktop Entry]\nName=${TICKER}\nExec=/usr/bin/${TICKER}\nIcon=/usr/share/applications/bitcoin.png\nType=Application\nCategories=Finance;\" > debian/wallet/usr/share/applications/app.desktop
+
+	dh_gencontrol
+	dh_builddeb
+" >> debian/rules
+
+# add changelog
+
+
+#build package
+dpkg-buildpackage -us -uc -b
