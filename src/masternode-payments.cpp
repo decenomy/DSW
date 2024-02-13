409c888945577489d6c17bd21d54ee5b97997c15
lucas <lucas.ua.eet@gmail.com>
2024-02-13 13:08:11 +0000

    removes code associated to SPORK113

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -252,17 +252,6 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
     const bool isPoSActive = Params().GetConsensus().NetworkUpgradeActive(nBlockHeight, Consensus::UPGRADE_POS);
     const CTransaction& txNew = (isPoSActive ? block.vtx[1] : block.vtx[0]);
 
-    auto t = GetTime();
-
-    if((t - reconsiderWindowTime) > HOUR_IN_SECONDS) {  // shift the reconsider window at each hour
-        reconsiderWindowMin = GetRand() % 10;           // choose randomly from minute 0 to minute 9
-        reconsiderWindowTime = t;
-
-        for (auto it = mapRejectedBlocks.cbegin(); it != mapRejectedBlocks.cend();) { // clean up old entries
-            it = (GetAdjustedTime() - (*it).second) > DAY_IN_SECONDS ? mapRejectedBlocks.erase(it) : std::next(it);
-        }
-    }
-
     // if it's the mint block then verify if exists an output with the right amount and address
     if (consensus.nMintHeight == nBlockHeight) {
         LogPrint(BCLog::MASTERNODE, "masternode", "IsBlockPayeeValid: Check mint reward\n");
@@ -291,17 +280,9 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
     // fails if spork 8 is enabled and
     // spork 113 is disabled or current time is outside the reconsider window
     if (sporkManager.IsSporkActive(SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT)) {
-        if (!sporkManager.IsSporkActive(SPORK_113_RECONSIDER_WINDOW_ENFORCEMENT))
-        {
-            return false;
-        }
-
-        if ((t / MINUTE_IN_SECONDS) % 10 != reconsiderWindowMin)
-        {
-            return false;
-        }
-
-        LogPrint(BCLog::MASTERNODE,"Masternode payment enforcement reconsidered, accepting block\n");
+        return false;
+    } else {
+        LogPrint(BCLog::MASTERNODE,"Masternode payment enforcement is disabled, accepting block\n");
         return true;
     }
 }
