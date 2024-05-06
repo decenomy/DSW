5a76ea5266052622ecc0e68a37ecd9e36c563226
pedro-at-decenomy <pedro@decenomy.net>
2024-05-06 12:38:17 +0100

    update the boost library to version 1.85

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,10 +1,9 @@
 package=boost
-$(package)_version=1_71_0
+$(package)_version=1_85_0
 # $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
 $(package)_download_path=https://github.com/decenomy/depends/raw/main/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
-$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
-$(package)_patches=fix_gcc_11_3_compile.patch
+$(package)_sha256_hash=7009fe1faa1697476bdc7027703a2badb84e849b7b0baad5086b087b971f8617
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
@@ -31,8 +30,7 @@ $(package)_cxxflags_linux=-fPIC
 endef
 
 define $(package)_preprocess_cmds
-  echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam && \
-  patch -p1 -i $($(package)_patch_dir)/fix_gcc_11_3_compile.patch
+  echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
 define $(package)_config_cmds
