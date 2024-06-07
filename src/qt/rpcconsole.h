4d0c06cd835482c3230dc303ed9d85b9617ff221
zimbora <lucas.ua.eet@gmail.com>
2024-06-07 13:09:35 +0100

    New feature: App update

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -83,6 +83,7 @@ public Q_SLOTS:
     void walletReindex();
     void walletResync();
     void walletBootstrap();
+    void walletUpdate();
 
     void reject();
     void message(int category, const QString &msg) { message(category, msg, false); }
