a32ac9d690c4ee2656d832778d7f3827566b3ffd
pedro-at-decenomy <pedro@decenomy.net>
2024-10-02 18:35:02 +0100

    small setWallet erase improvement

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1971,8 +1971,11 @@ bool CWallet::AvailableCoins(std::vector<COutput>* pCoins,      // --> populates
         }
     }
 
-    for (auto it = vErase.begin(); it != vErase.end(); ++it) {
-        setWallet.erase(*it);
+    if(vErase.size() > 0) {
+        for (auto& h : vErase) {
+            setWallet.erase(h);
+        }
+        setWallet.rehash(0);
     }
 
     return (pCoins && pCoins->size() > 0);
